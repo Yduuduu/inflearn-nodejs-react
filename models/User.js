dda9341da4f43a2e8c73e65904cf1f864776edd8
Yduuduu <hou_o@naver.com>
2022-05-04 10:23:41 +0900

    Bcrypt, jsonwebtoken, Auth

--- models/User.js
+++ models/User.js
@@ -1,4 +1,7 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+var jwt = require('jsonwebtoken');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -30,7 +33,67 @@ const userSchema = mongoose.Schema({
         type: Number
     }
 })
+userSchema.pre('save', function(next){
+    var user = this;
+
+    if(user.isModified('password')){
+        //비밀번호를 암호화 시킨다.
+
+        bcrypt.genSalt(saltRounds, function(err, salt) {
+            if(err) return next(err)
+            bcrypt.hash(user.password, salt, function(err, hash) {
+                // myPlaintextPassword == 사용자가 입력한 비밀번호 그 자체
+                if(err) return next(err)
+                user.password = hash
+                next()
+            });
+        });
+    } else {
+        next()
+    }
+})
+
+userSchema.methods.comparePassword = function(plainPassword, cb){
+    //plainPassword 1234567 암호화된 비밀번호 $2b$10$yXWl688m9QheFHcSxZl5G.LyDSb1hstiyphbbyPVg.1tMegaNKssK
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return cb(err);
+        cb(null, isMatch);
+    })
+}
+
+userSchema.methods.generateToken = function(cb){
+    var user = this;
+
+    //jsonwebtoken을 이용하여 token을 생성하기
+
+    var token = jwt.sign(user._id.toHexString(), 'secretToken')
+
+    /* user._id + 'secretToken' = token
+    ->
+    'secretToken' -> user._id */
+    user.token = token
+    user.save(function(err, user){
+        if(err) return cb(err);
+        cb(null, user);
+    })
+}
+
+userSchema.statics.findByToken = function(token, cb){
+    var user = this;
+    
+    // user._id + '' = token
+    // 토큰을 decode 한다.
+    jwt.verify(token, 'secretToken', function(err, decoded){
+        // 유저 아이디를 이용해서 유저를 찾은 다음에
+        // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는 지 확인
+
+        user.findOne({"_id": decoded, "token": token }, function(err, user){
+            if (err) return cd(err);
+            cb(null, user)
+        })
+    });
+}
+
 const User = mongoose.model('User', userSchema)
 
-
-module.exports = {User}
+module.exports = { User };
